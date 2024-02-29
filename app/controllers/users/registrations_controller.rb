5ac98dc4d9e632520d8edf1704cf3c8c2e68af1e
Thinus Van De Venter <thinusvdv2001@gmail.com>
2024-02-29 16:54:11 +0200

    Setup jwt rails api

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,20 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    sign_in(resource_name, resource)
+    render json: resource
+  end
+end
+# app/users/sessions_controller.rb
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  private
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+  def respond_to_on_destroy
+    render json: { message: "Logged out." }
+  end
+end
